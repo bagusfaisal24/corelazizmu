f9e3a51c6d9ad23ee954b2240cb30121b668fa90
bagus faisal kh <bagus.faizal@dssconsulting.id>
2020-07-08 17:00:15 +0700

    remove default reason

--- src/main/java/portal/core/exception/DataNotFoundException.java
+++ src/main/java/portal/core/exception/DataNotFoundException.java
@@ -6,14 +6,14 @@ import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Data Not Found")
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 @NoArgsConstructor
 public class DataNotFoundException extends RuntimeException {
 
     @Getter
     @Setter
     private String message = "Data Not Found";
-
+    
     public DataNotFoundException(String message) {
         super();
         this.message = message;
