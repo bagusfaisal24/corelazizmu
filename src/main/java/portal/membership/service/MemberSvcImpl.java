cbfb8eb5afd57783f9552e9302873dc81530a623
bagus faisal kh <bagus.faizal@dssconsulting.id>
2020-07-08 22:09:43 +0700

    fix set NBM

--- src/main/java/portal/membership/service/MemberSvcImpl.java
+++ src/main/java/portal/membership/service/MemberSvcImpl.java
@@ -14,54 +14,53 @@ import java.time.LocalDate;
 
 @Service
 public class MemberSvcImpl implements MemberSvc {
-
-    private MemberRepositories memberRepositories;
-    private RantingSvc rantingSvc;
-    private TransactionDate transactionDate;
-
-    @Autowired
-    public MemberSvcImpl(MemberRepositories memberRepositories, RantingSvc rantingSvc,
-                         TransactionDate transactionDate) {
-        this.memberRepositories = memberRepositories;
-        this.rantingSvc = rantingSvc;
-        this.transactionDate = transactionDate;
-    }
-
-    @Override
-    public Iterable<MemberModel> findAll() {
-        return memberRepositories.findAll();
-    }
-
-    @Override
-    public MemberModel createNew(MemberForm form) {
-        RantingModel ranting = rantingSvc.findOne(form.getRantingId());
-        if (ranting == null) throw new DataNotFoundException("data ranting tidak ada");
-
-        MemberModel member = new MemberModel();
-        member.setName(form.getName());
-        member.setBirthDate(transactionDate.getStartOfDay(form.getBirthDate()));
-        member.setDobPlace(form.getDobPlace());
-        member.setJob(form.getJob());
-        member.setRanting(ranting);
-        return memberRepositories.save(member);
-    }
-
-    @Override
-    public MemberModel getDetail(Long id) {
-        return memberRepositories.findOne(id);
-    }
-
-    @Override
-    public MemberModel update(Long id, MemberForm form) {
-        RantingModel ranting = rantingSvc.findOne(form.getRantingId());
-        if (ranting == null) throw new DataNotFoundException("data ranting tidak ada");
-        MemberModel member = getDetail(id);
-        member.setName(form.getName());
-        member.setBirthDate(transactionDate.getStartOfDay(form.getBirthDate()));
-        member.setDobPlace(form.getDobPlace());
-        member.setJob(form.getJob());
-        member.setNbm(form.getNbm());
-        member.setRanting(ranting);
-        return memberRepositories.save(member);
-    }
+	
+	private MemberRepositories memberRepositories;
+	private RantingSvc rantingSvc;
+	private TransactionDate transactionDate;
+	
+	@Autowired
+	public MemberSvcImpl(MemberRepositories memberRepositories, RantingSvc rantingSvc,
+						 TransactionDate transactionDate) {
+		this.memberRepositories = memberRepositories;
+		this.rantingSvc = rantingSvc;
+		this.transactionDate = transactionDate;
+	}
+	
+	@Override
+	public Iterable<MemberModel> findAll() {
+		return memberRepositories.findAll();
+	}
+	
+	@Override
+	public MemberModel createNew(MemberForm form) {
+		RantingModel ranting = rantingSvc.findOne(form.getRantingId());
+		if (ranting == null) throw new DataNotFoundException("data ranting tidak ada");
+		
+		MemberModel member = new MemberModel();
+		return setMember(form, ranting, member);
+	}
+	
+	private MemberModel setMember(MemberForm form, RantingModel ranting, MemberModel member) {
+		member.setName(form.getName());
+		member.setBirthDate(transactionDate.getStartOfDay(form.getBirthDate()));
+		member.setDobPlace(form.getDobPlace());
+		member.setJob(form.getJob());
+		member.setNbm(form.getNbm());
+		member.setRanting(ranting);
+		return memberRepositories.save(member);
+	}
+	
+	@Override
+	public MemberModel getDetail(Long id) {
+		return memberRepositories.findOne(id);
+	}
+	
+	@Override
+	public MemberModel update(Long id, MemberForm form) {
+		RantingModel ranting = rantingSvc.findOne(form.getRantingId());
+		if (ranting == null) throw new DataNotFoundException("data ranting tidak ada");
+		MemberModel member = getDetail(id);
+		return setMember(form, ranting, member);
+	}
 }
